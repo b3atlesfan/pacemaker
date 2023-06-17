66fb08181a004951117ff5e6c49affa12ccad3c0
Julian Geheeb <geheeb@in.tum.de>
2023-06-17 14:18:45 +0200

    Added VueFlow example

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,30 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import VueFlowExample from "@/components/VueFlowExample.vue";
+import TheWelcome from "@/components/TheWelcome.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <VueFlowExample />
   </main>
 </template>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  //flex-direction: column;
+  //max-width: 500px;
+  //width: 100%;
+  //margin: 0 auto;
+  //padding: 40px 16px;
+  border-width: 3px;
+  border-style: solid;
+  border-color: #2c3e50;
+
+  .border {
+    border-width: 3px;
+    border-style: solid;
+    border-color: #2c3e50;
+  }
+}
+</style>
