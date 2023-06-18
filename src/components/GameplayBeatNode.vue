31143fec26798de71775fc881cdf9b102825bec3
Julian Geheeb <geheeb@in.tum.de>
2023-06-18 14:42:25 +0200

    Auto-formatted main.css

--- src/components/GameplayBeatNode.vue
+++ src/components/GameplayBeatNode.vue
@@ -30,19 +30,28 @@ const props = defineProps({
       <li>{{ props.label }}</li>
       <li>Difficulty is {{ props.data }} / 10</li>
     </ul>
+    <Handle id="a_in" type="target" :position="Position.Right" class="gameplay-beat-handle"/>
+    <Handle id="a_out" type="source" :position="Position.Right" class="gameplay-beat-handle"/>
+    <Handle id="b" type="source" :position="Position.Top" class="gameplay-beat-handle"/>
+    <Handle id="c" type="source" :position="Position.Bottom" class="gameplay-beat-handle"/>
+    <Handle id="d" type="source" :position="Position.Left" class="gameplay-beat-handle"/>
   </div>
-  <Handle id="a_in" type="target" :position="Position.Right" class="gameplay-beat-handle"/>
-  <Handle id="a_out" type="source" :position="Position.Right" class="gameplay-beat-handle"/>
-  <Handle id="b" type="source" :position="Position.Top" class="gameplay-beat-handle"/>
-  <Handle id="c" type="source" :position="Position.Bottom" class="gameplay-beat-handle"/>
-  <Handle id="d" type="source" :position="Position.Left" class="gameplay-beat-handle"/>
 </template>
 
 <style scoped>
 .gameplay-beat-node {
+  /*
   background: #9CA8B3;
   color: #fff;
   padding: 10px;
+  border-width: 3px;
+  border-color: #6f3381;
+  border-radius: 90%;
+   */
+  margin: 3px;
+  border-width: 3px;
+  border-color: #6f3381;
+  border-radius: 90%;
 }
 
 .gameplay-beat-handle {
