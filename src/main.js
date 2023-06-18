73113d9dda03b2aaa5a4acade8138d2a0d4bb92e
Julian Geheeb <geheeb@in.tum.de>
2023-06-18 11:59:39 +0200

    Added context menu package

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import './assets/main.css'
+import '@imengyu/vue3-context-menu/lib/vue3-context-menu.css'
+import ContextMenu from '@imengyu/vue3-context-menu'
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -6,6 +8,7 @@ import router from './router'
 
 const app = createApp(App)
 
+app.use(ContextMenu)
 app.use(router)
 
 app.mount('#app')
