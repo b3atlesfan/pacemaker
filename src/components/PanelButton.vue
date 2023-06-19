97f27187502d5f9e65c2ef772db7df2f430d8725
Julian Geheeb <geheeb@in.tum.de>
2023-06-19 10:24:17 +0200

    Implemented light and dark mode for PanelButton

--- src/components/PanelButton.vue
+++ src/components/PanelButton.vue
@@ -1,7 +1,19 @@
 <script setup>
 
 const props = defineProps({
+  dark: {
+    type: Boolean,
+    required: true
+  },
   backgroundColor: {
+    type: String,
+    required: false
+  },
+  backgroundColorLight: {
+    type: String,
+    required: false
+  },
+  backgroundColorDark: {
     type: String,
     required: false
   },
@@ -25,33 +37,55 @@ const props = defineProps({
     type: String,
     required: false,
   },
+  fillColorLight: {
+    type: String,
+    required: false,
+  },
+  fillColorDark: {
+    type: String,
+    required: false,
+  },
   svgString: {
+    type: String,
+    required: false,
+  },
+  svgStringDark: {
     type: String,
     required: false,
   }
 })
 
-const style = "background-color: " + (props.backgroundColor ?? "#ffffff")
+const emit = defineEmits(["onClick"])
+
+const baseSvgString = "M12 17q-2.075 0-3.537-1.463Q7 14.075 7 12t1.463-3.538Q9.925 7 12 7t3.538 1.462Q17 9.925 17 12q0 2.075-1.462 3.537Q14.075 17 12 17ZM2 13q-.425 0-.712-.288Q1"
+
+const color_light = props.backgroundColor ?? (props.backgroundColorLight ?? "#fffffb")
+const color_dark = props.backgroundColor ?? (props.backgroundColorDark ?? "#292524")
 const title = props.title ?? "Button"
 const width = props.width ?? 16
 const height = props.height ?? 16
 const viewBox = props.viewBox ?? "0 0 24 24"
-const fill = props.fillColor ?? "#000"
-const d = props.svgString ?? "M12 17q-2.075 0-3.537-1.463Q7 14.075 7 12t1.463-3.538Q9.925 7 12 7t3.538 1.462Q17 9.925 17 12q0 2.075-1.462 3.537Q14.075 17 12 17ZM2 13q-.425 0-.712-.288Q1"
-
-
-const emit = defineEmits(["onClick"])
-
-function onClickCallback() {
-  console.log(props.title, props.fill, props.d)
-  emit("onClick")
-}
+const fill = props.fillColor ?? (props.fillColorLight ?? "#292524")
+const fill_dark = props.fillColor ?? (props.fillColorDark ?? "#fffffb")
+const d = props.svgString ?? baseSvgString
+const d_dark = props.svgStringDark ?? (props.svgString ?? baseSvgString)
 </script>
 
 <template>
-  <button :style=style :title=title @click="onClickCallback">
-    <svg :width=width :height=height :viewBox=viewBox>
-      <path :fill=fill :d=d />
-    </svg>
+  <!--
+  <button :style="{{ dark ? color_dark : color_light }}" :title=title @click="$emit('onClick')">
+  -->
+  <button :style="{ backgroundColor: dark ? color_dark : color_light }" :title=title @click="$emit('onClick')">
+    <template v-if="dark">
+      <svg :width=width :height=height :viewBox=viewBox>
+        <path :fill=fill_dark :d=d_dark />
+      </svg>
+    </template>
+
+    <template v-else >
+      <svg :width=width :height=height :viewBox=viewBox>
+        <path :fill=fill :d=d />
+      </svg>
+    </template>
   </button>
 </template>
