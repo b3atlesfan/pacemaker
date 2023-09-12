b8b8440e8021e6092d01feeaa69f1847c0881072
Julian Geheeb <geheeb@in.tum.de>
2023-09-12 14:14:38 +0200

    Imported vuetify

--- src/main.js
+++ src/main.js
@@ -10,11 +10,23 @@ import { createPinia } from "pinia";
 import App from './App.vue'
 import router from './router'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 const app = createApp(App)
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
 
 app.use(ContextMenu)
 app.use(router)
 app.use(createVfm())
 app.use(createPinia())
+app.use(vuetify)
 
 app.mount('#app')
