116b55eb02cf1eab74ac5dd6d9134031f09c7c95
Julian Geheeb <geheeb@in.tum.de>
2023-11-19 13:37:29 +0100

    Divided by 2

--- src/components/ContentGraph.vue
+++ src/components/ContentGraph.vue
@@ -33,7 +33,9 @@ const computeOptions = {
 function computeBeat(): {name: string, data: {x: string | number, y: number | null}[]}[] | undefined {
   return props.paths.map(path => {
     return { name: path.name, data: path.path.map((content, id) => {
-      const intensity = content ? (content.intensity + content.narrativeIntensity) * 0.5 : null
+      const intensity = content ? (content.intensity + content.narrativeIntensity) / 2 : null
+      console.log("gameplay " + content?.intensity)
+      console.log("narrative " + content?.narrativeIntensity)
       console.log("intensity " + intensity)
       return {x: id, y: intensity}
     })
