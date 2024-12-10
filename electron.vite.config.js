a61b5cb2b3cfc5ea6202dc55923a548097e860ba
Michael Dey <michael.dey@validas.de>
2024-12-10 16:50:36 +0100

    Update electron.vite.config.js

--- electron.vite.config.js
+++ electron.vite.config.js
@@ -1,12 +1,54 @@
 import { defineConfig } from "electron-vite";
 import react from '@vitejs/plugin-react';
-import vue from '@vitejs/plugin-vue';
+// Plugins
+import vue from '@vitejs/plugin-vue'
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import ViteFonts from 'unplugin-fonts/vite'
+
+// Utilities
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   publicDir: false,
   main: {},
   preload: {},
-  renderer: {
-    plugins: [vue()]
+  renderer: {plugins: [
+    vue({
+      template: { transformAssetUrls }
+    }),
+    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+    vuetify({
+      autoImport: true,
+      styles: {
+        configFile: 'src/styles/settings.scss',
+      },
+    }),
+    ViteFonts({
+      google: {
+        families: [{
+          name: 'Roboto',
+          styles: 'wght@100;300;400;500;700;900',
+        }],
+      },
+    }),
+  ],
+  define: { 'process.env': {} },
+  resolve: {
+    alias: {
+      //'@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+    extensions: [
+      '.js',
+      '.json',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+      '.vue',
+    ],
+  },
+  server: {
+    port: 5173,
+  },
   },
 });
