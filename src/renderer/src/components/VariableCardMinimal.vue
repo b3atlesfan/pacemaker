7c08e36ff6c03182b862a5e1b5ac50ab6fb75a0c
Michael Dey <michael.dey@validas.de>
2025-03-18 18:43:48 +0100

    Create VariableCardMinimal.vue

---
+++ src/renderer/src/components/VariableCardMinimal.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { defineProps, watch } from 'vue';
+import { useTheme } from 'vuetify';
+const theme = useTheme();
+
+const props = defineProps({
+    currentVariable: Object,
+});
+
+// on changes of props emit the event to update the variable
+
+watch(props.currentVariable, (newValue, oldValue) => {
+    emit('onUpdateVariable', newValue);
+});
+
+const emit = defineEmits(['onUpdateVariable']);
+
+</script>
+
+<template>
+    <v-card>
+        <v-row class="d-flex align-center justify-left" style="margin-left: 10px;">
+            <v-col cols="2">{{ props.currentVariable.name }}</v-col>
+            <v-col cols="2">
+                <v-text-field v-model="props.currentVariable.intensityWeight" type="number" density="compact" hide-details="auto"></v-text-field>
+            </v-col>
+            <v-col cols="2">
+                <v-text-field v-model="props.currentVariable.narrativeWeight" type="number" density="compact" hide-details="auto"></v-text-field>
+            </v-col>
+            <v-col cols="2">
+                <v-checkbox v-model="props.currentVariable.isMultiplier" label="Multiplier"></v-checkbox>
+            </v-col>
+        </v-row>
+    </v-card>
+</template>
